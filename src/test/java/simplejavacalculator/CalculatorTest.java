b2733097383e3c820d2c0024be605fd74dd52c3a
dikhayeva00 <dikhayeva00@gmail.com>
2020-06-17 19:30:09 +0900

    new plugin

---
+++ src/test/java/simplejavacalculator/CalculatorTest.java
@@ -0,0 +1,16 @@
+package simplejavacalculator;
+
+        import org.junit.Assert;
+        import org.junit.Test;
+        import static org.junit.Assert.*;
+        import static java.lang.Double.NaN;
+
+public class CalculatorTest {
+    @Test
+    public void Testing0() {
+        Calculator c = new Calculator();
+        Double result = c.calculateMono(Calculator.MonoOperatorModes.square, 5.0);
+        Assert.assertEquals((Double) 25.0, result);
+    }
+}
+
