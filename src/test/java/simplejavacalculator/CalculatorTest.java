61491323fe0c0b74f1c4ff2ae2b2feb7e0618276
Chitipat Phetmunee <xtipat@gmail.com>
2020-06-19 17:10:46 +0700

    change name squareRoot

--- src/test/java/simplejavacalculator/CalculatorTest.java
+++ src/test/java/simplejavacalculator/CalculatorTest.java
@@ -6,11 +6,19 @@ package simplejavacalculator;
         import static java.lang.Double.NaN;
 
 public class CalculatorTest {
+    //square, oneDevidedBy, cos, sin, tan ,log , rate, abs
     @Test
     public void Testing_square_1() {
         Calculator c = new Calculator();
         Double result = c.calculateMono(Calculator.MonoOperatorModes.square, 5.0);
         Assert.assertEquals((Double) 25.0, result);
+    }
+    // squareRoot
+    @Test
+    public void Testing_squareRoot_1() {
+        Calculator c = new Calculator();
+        Double result = c.calculateMono(Calculator.MonoOperatorModes.square, 49.0);
+        Assert.assertEquals((Double) 7.0, result);
     }
     @Test
     public void Testing_log_1() {
